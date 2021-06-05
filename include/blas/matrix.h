255bafaea93b0a16e1eae257a6205c9d919e36c3
jiang <392711804@qq.com>
2021-06-05 17:05:42 +0800

    modify matrix.h

--- include/blas/matrix.h
+++ include/blas/matrix.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <cstring>
+#include <emmintrin.h>
 
 namespace blas {
+
 template<typename T, std::size_t R, std::size_t C>
 class Matrix {
 
@@ -29,14 +31,13 @@ class Matrix {
     return m;
   }
 
-
   Matrix<T, R, C> operator*(T num) {
     Matrix<T, R, C> m = *this;
-     for (int i = 0; i < R; ++i) {
-       for (int j = 0; j < C; ++j) {
-         m[i][j] *= num;
-       }
-     }
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < C; ++j) {
+        m[i][j] *= num;
+      }
+    }
     return m;
   }
 
@@ -49,16 +50,73 @@ class Matrix {
     return *this;
   }
 
-  T *begin() {
-    return &data_[0][0];
+  auto begin() -> T (*)[C] {
+    return &data_[0];
   }
 
-  T *end() {
-    return &data_[R - 1][C];
+  auto end() -> T (*)[C] {
+    return &data_[R - 1];
   }
 
  private:
   T data_[R][C];
+};
+
+template<std::size_t R, std::size_t C>
+class Matrix<float, R, C> {
+ public:
+  explicit Matrix(const float (&list)[R][C]) {
+    memcpy(data_, list, sizeof(float) * R * C);
+  }
+
+  Matrix() = default;
+  float *operator[](std::size_t idx) {
+    return &data_[idx][0];
+  }
+
+  Matrix<float, R, C> &operator*=(float num) {
+    const __m128 scalar = _mm_set1_ps(num);
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < R; j += 4) {
+        __m128 res = _mm_mul_ps(_mm_set_ps(data_[i][j + 3], data_[i][j + 2], data_[i][j + 1], data_[i][j]), scalar);
+        memcpy(&data_[i][j], &res, sizeof(res));
+      }
+      switch (C % 4) {
+        case 3:data_[i][C - 3] *= num;
+        case 2:data_[i][C - 2] *= num;
+        case 1:data_[i][C - 1] *= num;
+      }
+    }
+    return *this;
+  }
+
+  Matrix<float, R, C> &operator+=(float num) {
+    const __m128 scalar = _mm_set1_ps(num);
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < R; j += 4) {
+        __m128 res = _mm_add_ps(_mm_set_ps(data_[i][j + 3], data_[i][j + 2], data_[i][j + 1], data_[i][j]), scalar);
+        memcpy(&data_[i][j], &res, sizeof(res));
+      }
+      switch (C % 4) {
+        case 3:data_[i][C - 3] += num;
+        case 2:data_[i][C - 2] += num;
+        case 1:data_[i][C - 1] += num;
+      }
+    }
+    return *this;
+  }
+
+  auto begin() -> float (*)[C] {
+    return &data_[0];
+  }
+
+  auto end() -> float (*)[C] {
+    return &data_[R - 1];
+  }
+
+ private:
+  float data_[R][C];
+
 };
 
 }
